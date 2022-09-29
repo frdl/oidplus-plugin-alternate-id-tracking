9cc9e4c0597261f6cec2dfc26636c28e6616eb12
Till Wehowski <github@webfan.de>
2022-09-29 20:58:06 +0200

    Update OIDplusPagePublicAltIds.class.php

--- plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
+++ plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
@@ -6,16 +6,22 @@
  *
  * Licensed under the MIT License.
  */
-
+/*
+*  ToDo: Cache proxy (to canonical) response?
+*        proxy (to canonical) if POST/PUT/DELETE Method or if CLI?
+*        DONE: change cache to "per ID -" or getAlternativesForQuery($id) Cache instead of the entire DB?
+*/
 if (!defined('INSIDE_OIDPLUS')) die();
 
 class OIDplusPagePublicAltIds extends OIDplusPagePluginPublic {
 
 
-	protected static $cache = null;
+	
+    protected static $cache = null;
+    protected static $caches = [];
 	
     public static function __callStatic($name, $arguments){
-		return call_user_func_array([new self, str_replace('Static', '', $name)], $arguments);
+       return call_user_func_array([new self, str_replace('Static', '', $name)], $arguments);
     }	
 	
 	public function action($actionID, $params) {
@@ -23,27 +29,28 @@ class OIDplusPagePublicAltIds extends OIDplusPagePluginPublic {
 	}
 
 	
-	public function init($html=true) {
+	public function init($html=true) {  
        if('GET' === $_SERVER['REQUEST_METHOD'] &&
 		  ( 'rdap.php' === basename($_SERVER['PHP_SELF']) || 'webwhois.php' === basename($_SERVER['PHP_SELF'])) && isset($_GET['query'])){
-		   $canonical = $this->getCanonical($_GET['query']);
+		   $canonical = $this->getCanonical($_GET['query']);			 
 		   if(false !== $canonical && $canonical !== $_GET['query']){
 			  // header('Location: //'.$_SERVER['SERVER_NAME'].str_replace($_GET['query'], $canonical, $_SERVER['REQUEST_URI']));
 			  // exit;
 			   $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
 			   $url = $protocol.'://'.$_SERVER['SERVER_NAME'].str_replace($_GET['query'], $canonical, $_SERVER['REQUEST_URI']);
-	           $opts =[
-                'http'=>[
-                    'method'=>$_SERVER['REQUEST_METHOD'],
-                     //'header'=>"Accept-Encoding: deflate, gzip\r\n",
-                   ],	
-	          ];
+	          
+			   $opts =[              
+				   'http'=>[                   
+					   'method'=>$_SERVER['REQUEST_METHOD'],                   
+					   //'header'=>"Accept-Encoding: deflate, gzip\r\n",                
+				   ],		        
+			   ];
 			    $context = stream_context_create($opts);
 			    $result = @file_get_contents($url, false, $context);
 			   if(false!==$result){	      
-				   foreach($http_response_header as $i => $header){
-                      header($header);
-                   }	
+				   foreach($http_response_header as $i => $header){                   
+					header($header);                  
+				   }	
 				   echo $result;
 				   exit;
 			   }
@@ -88,27 +95,51 @@ class OIDplusPagePublicAltIds extends OIDplusPagePluginPublic {
                         }
                 }
               
-			self::$cache = array($alt_ids, $rev_lookup);
-			return self::$cache;
+			
+		self::$cache = array($alt_ids, $rev_lookup);		
+		return self::$cache;
         }
 	
 	
-       public function getAlternativesForQuery($id) {		   
-             list($alt_ids, $rev_lookup) = $this->readAll(false);
+       public function getAlternativesForQuery($id/* 1.3.6.1.4.1.37476.2.5.2.3.7 signature takes just 1 param!? , $noCache = false*/) {				
+	       if(/*true !== $noCache && */isset(self::$caches[$id]) ){			  
+		    return self::$caches[$id];				
+	       }	   
+     
+			 list($ns, $altIdRaw) = explode(':', $id, 2);
+			 if($ns === 'weid'){
+				$id='oid:'.\WeidOidConverter::weid2oid($id);
+			 }	   	   
+		 
+	       list($alt_ids, $rev_lookup) = $this->readAll(false);
 		   
-		     $res = [];
+		     $res = [
+			   $id,
+			 ];
 		     if(isset($rev_lookup[$id])){
 				 $res = array_merge($res, $rev_lookup[$id]);
-			 }
+		     }
 		    foreach($alt_ids as $original => $altIds){
 				if($id === $original || in_array($id, $altIds) ){
 					 $res = array_merge($res, $altIds);
 					 $res = array_merge($res, [$original]);
 				}
 			}
-		   
-		   $res = array_unique($res);
+			
+		   $weid = false;
+		   foreach($res as $alt){
+			 list($ns, $altIdRaw) = explode(':', $alt, 2);			
+			 if($ns === 'oid'){
+				$weid=\WeidOidConverter::oid2weid($altIdRaw);
+			 }	   
+		   }
 
+		   if(false !== $weid){
+			 $res[]=$weid;   
+		   }
+		   $res = array_unique($res);		   		   
+               
+		   self::$caches[$id] = $res;
            return $res;
         }
 	
