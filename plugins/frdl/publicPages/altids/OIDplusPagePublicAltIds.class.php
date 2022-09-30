44dbbb0fe0ede4b8b76b98e22bfc7f85ccb30404
Till Wehowski <github@webfan.de>
2022-09-30 09:17:38 +0200

    Update OIDplusPagePublicAltIds.class.php

--- plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
+++ plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
@@ -30,7 +30,7 @@ class OIDplusPagePublicAltIds extends OIDplusPagePluginPublic {
 
 	
 	public function init($html=true) {  
-       if('GET' === $_SERVER['REQUEST_METHOD'] &&
+       if(true===$html && 'GET' === $_SERVER['REQUEST_METHOD'] &&
 		  ( 'rdap.php' === basename($_SERVER['PHP_SELF']) || 'webwhois.php' === basename($_SERVER['PHP_SELF'])) && isset($_GET['query'])){
 		   $canonical = $this->getCanonical($_GET['query']);			 
 		   if(false !== $canonical && $canonical !== $_GET['query']){
