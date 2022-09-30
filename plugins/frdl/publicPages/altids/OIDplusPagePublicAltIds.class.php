50c1c4fb0970216e20e6ae7b40ea9de6fb99c46a
Daniel Marschall <28412477+danielmarschall@users.noreply.github.com>
2022-09-30 23:41:14 +0200

    Update OIDplusPagePublicAltIds.class.php
    ( rejected #12 )

--- plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
+++ plugins/frdl/publicPages/altids/OIDplusPagePublicAltIds.class.php
@@ -28,46 +28,6 @@ class OIDplusPagePublicAltIds extends OIDplusPagePluginPublic {
 
 	}
 
-	
-	public function init($html=true) {  
-		
-		
-		/**
-		*  This does not work correctly, due to overload/requests itself???
-		**/ 
-		/*
-		
-       if('cli' !== strtolower(\PHP_SAPI) && true===$html && 'GET' === $_SERVER['REQUEST_METHOD'] &&
-		  ( 'rdap.php' === basename($_SERVER['PHP_SELF']) ||
-		   'webwhois.php' === basename($_SERVER['PHP_SELF'])) && isset($_GET['query'])){
-		   $canonical = $this->getCanonical($_GET['query']);		 
-		   if(false !== $canonical && $canonical !== $_GET['query']){
-			     
-			  // header('Location: //'.$_SERVER['SERVER_NAME'].str_replace($_GET['query'], $canonical, $_SERVER['REQUEST_URI']));
-			  // exit;
-			   $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
-			   $url = $protocol.'://'.$_SERVER['SERVER_NAME'].str_replace($_GET['query'], $canonical, $_SERVER['REQUEST_URI']);
-	          
-			   $opts =[              
-				   'http'=>[                   
-					   'method'=>$_SERVER['REQUEST_METHOD'],                   
-					   //'header'=>"Accept-Encoding: deflate, gzip\r\n",                
-				   ],		        
-			   ];
-			    $context = stream_context_create($opts);
-			    $result = @file_get_contents($url, false, $context);
-			   if(false!==$result){	      
-				   foreach($http_response_header as $i => $header){                   
-					header($header);                  
-				   }	
-				   echo $result;
-				   exit;
-			   }
-		   }		  
-	   } 
-	   */
-	}
-
 	public function gui($id, &$out, &$handled) {
 	
 	}
